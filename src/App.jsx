089e054499cc52c102039fdc5e43f42e5f4d5563
Ayaanleahmed <boqolhaji5@gmail.com>
2024-05-26 14:41:54 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 
 
 const ApiTest ="https://jsonplaceholder.typicode.com/posts/1"
+const ApiTest1 = "https://jsonplaceholder.typicode.com/posts/2"
 function App() {
 
   const [Read ,setRead] = useState(null)
@@ -15,9 +16,41 @@ function App() {
   fetchdata();
   
   })
+
+
+
+
+  const [post ,setPost] = useState(null)
+  useEffect(()=>{
+  const fetchdata1= async () =>{
+    const result1 = await fetch(ApiTest1);
+    const data1 =  await result1.json();
+    setPost(data1)
+
+  }
+  fetchdata1();
+  
+  })
   return (
     <>
-    <h1>{Read?.body}</h1>
+    <div className="vvvvvvvvvvvv">
+    <div className="nnn">
+    <img src="id.jpg" alt=""width={100} height={100} />
+    <h1 className="idn">{Read?.id}</h1>
+
+   
+     </div>
+     </div>
+
+     <div >
+      <h1 className="bbbbbbbbb"></h1>
+      <div className="lllll">
+     <img src="h.png" alt=""  height={100}/>
+     <h1 className="idn">{post?.id}</h1>
+     </div>
+     </div>
+     
+
     </>
   )
 }
